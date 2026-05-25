9f66f8d26b53fe640a5247c467fe77ceae7434c0
ocean <ocean.double.wings@gmail.com>
2026-05-25 10:14:22 +0900

    問題表示修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -65,7 +65,7 @@ if params[:ids].present?
   # idsの順番どおりに問題を並べる
   @questions = ids.map { |id| Question.find_by(id: id) }.compact
 else
- @questions = []
+@questions = base.order("RANDOM()").limit(count)
 end
 
 @index = params[:index].to_i

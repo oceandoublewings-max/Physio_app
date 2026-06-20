09311ce9ba16e14bfdb2135d7bb844b464247a04
ocean <ocean.double.wings@gmail.com>
2026-06-21 00:15:15 +0900

    remove debug

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -86,10 +86,6 @@ if @index >= @questions.length
 else
   @question = @questions[@index]
 
-Rails.logger.info "QUESTION: #{@question.inspect}"
-Rails.logger.info "CATEGORY: #{@question.category}"
-Rails.logger.info "QTYPE: #{@question.qtype}"
-Rails.logger.info "CHOICES: #{@question.choices}"
 end
 end
 

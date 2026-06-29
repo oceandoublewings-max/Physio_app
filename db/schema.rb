1c6d0a77f03a0a4b8f3b4505e4ba6d788ff66662
ocean <ocean.double.wings@gmail.com>
2026-06-29 19:12:10 +0900

    PostgreSQL migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2026_06_16_223015) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "questions", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false

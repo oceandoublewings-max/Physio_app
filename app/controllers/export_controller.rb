464bff7bdc3dab04dabac6c44784aaa669516333
ocean <ocean.double.wings@gmail.com>
2026-07-11 21:49:38 +0900

    Fix export controller

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -4,40 +4,41 @@ class ExportController < ApplicationController
     password: ENV.fetch("EXPORT_PASSWORD", "your_password")
   )
 
- def anatomy
-  questions = Question.where(category: "解剖学").order(:id)
-
-  text = "# 解剖学 ゴロ集\n\n"
-
-  questions.each do |q|
-    text << <<~MD
-    ---
-    問題ID: #{q.id}
-    問題: #{q.content}
-    答え: #{q.answer}
-    ゴロ:
-    国試ポイント:
-    #{q.explanation}
-
-    MD
+  def anatomy
+    export_markdown("解剖学", "anatomy")
   end
 
-  send_data text,
-            filename: "anatomy.md",
-            type: "text/markdown",
-            disposition: "attachment"
-end
-
-  send_data text,
-            filename: "anatomy.md",
-            type: "text/markdown"
-end
-
   def physiology
-    render plain: "あとで実装します"
+    export_markdown("生理学", "physiology")
   end
 
   def kinesiology
-    render plain: "あとで実装します"
+    export_markdown("運動学", "kinesiology")
+  end
+
+  private
+
+  def export_markdown(category, filename)
+    questions = Question.where(category: category).order(:id)
+
+    text = "# #{category} ゴロ集\n\n"
+
+    questions.each do |q|
+      text << <<~MD
+      ---
+      問題ID: #{q.id}
+      問題: #{q.content}
+      答え: #{q.answer}
+      ゴロ:
+      国試ポイント:
+      #{q.explanation}
+
+      MD
+    end
+
+    send_data text,
+              filename: "#{filename}.md",
+              type: "text/markdown",
+              disposition: "attachment"
   end
 end

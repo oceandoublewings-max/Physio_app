815612ed2ef4dc7c011cd7e25409083718a47954
ocean <ocean.double.wings@gmail.com>
2026-07-15 23:59:24 +0900

    Fix top button navigation after quiz

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::Base
   def require_login
     return if session[:user_id].present?
 
-    return if controller_name == "sessions"
+    # ログイン不要ページ
+    return if controller_name.in?(%w[home sessions])
 
     redirect_to login_path
   end

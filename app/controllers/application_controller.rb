291aecca41a5a5a0f83f1e0e957d0f5f1712a6ca
ocean <ocean.double.wings@gmail.com>
2026-07-17 07:54:07 +0900

    Allow omniauth callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
   before_action :require_login
 
   private
@@ -8,8 +7,8 @@ class ApplicationController < ActionController::Base
     return if current_user.present?
 
     return if controller_name == "sessions"
+    return if controller_name == "omniauth_callbacks"
 
     redirect_to login_path
   end
-
 end

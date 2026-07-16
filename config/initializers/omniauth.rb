1832f59720f5d7deaa28c330ee0d8518cfeb60bc
ocean <ocean.double.wings@gmail.com>
2026-07-17 00:37:40 +0900

    ゲストログインをDevise対応

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,40 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider(
-    :google_oauth2,
-    ENV.fetch("GOOGLE_CLIENT_ID"),
-    ENV.fetch("GOOGLE_CLIENT_SECRET"),
-    {
-      scope: "email,profile",
-      prompt: "select_account"
-    }
-  )
+class SessionsController < ApplicationController
+
+  def login
+  end
+
+  def create
+    auth = request.env["omniauth.auth"]
+
+    user = User.find_or_create_by(email: auth.info.email) do |u|
+      u.password = Devise.friendly_token[0, 20]
+    end
+
+    sign_in(user)
+
+    redirect_to root_path, notice: "Googleでログインしました！"
+  end
+
+  def guest_login
+    password = Devise.friendly_token[0, 20]
+
+    user = User.create!(
+      guest: true,
+      email: "guest_#{SecureRandom.uuid}@example.com",
+      password: password,
+      password_confirmation: password
+    )
+
+    session[:user_id] = user.id
+
+    redirect_to root_path, notice: "ゲストログインしました！"
+  end
+
+  def destroy
+    reset_session
+    sign_out(current_user) if user_signed_in?
+
+    redirect_to login_path, notice: "ログアウトしました。"
+  end
+
 end

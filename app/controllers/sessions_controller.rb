f43e188ca3f60064aa1c85fb9992d64a02fabd07
ocean <ocean.double.wings@gmail.com>
2026-07-16 12:08:57 +0900

    Add Google OAuth session login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,18 @@ class SessionsController < ApplicationController
   def login
   end
 
+  def create
+    auth = request.env["omniauth.auth"]
+
+    user = User.find_or_create_by(email: auth.info.email) do |u|
+      u.password = Devise.friendly_token[0, 20]
+    end
+
+    sign_in(user)
+
+    redirect_to root_path, notice: "Googleでログインしました！"
+  end
+
   def guest_login
     user = User.create!(guest: true)
 
@@ -13,6 +25,7 @@ class SessionsController < ApplicationController
 
   def destroy
     reset_session
+    sign_out(current_user) if user_signed_in?
 
     redirect_to login_path, notice: "ログアウトしました。"
   end

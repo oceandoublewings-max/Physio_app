2f98d5be3550c8f1bd2dd452ad914c73047255ea
ocean <ocean.double.wings@gmail.com>
2026-07-18 01:26:29 +0900

    Add OmniAuth error logging

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,28 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
+    Rails.logger.info "========== OMNIAUTH START =========="
+    Rails.logger.info request.env["omniauth.auth"].inspect
+
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
+    Rails.logger.info "USER: #{@user.inspect}"
+
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+      sign_in @user
+      redirect_to root_path, notice: "Googleでログインしました。"
     else
+      Rails.logger.error @user.errors.full_messages.join(", ")
+
       session["devise.google_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
+  rescue => e
+    Rails.logger.error "========== OMNIAUTH ERROR =========="
+    Rails.logger.error e.class.to_s
+    Rails.logger.error e.message
+    Rails.logger.error e.backtrace.join("\n")
+
+    redirect_to new_user_session_path,
+                alert: "Googleログイン中にエラーが発生しました。"
   end
 end

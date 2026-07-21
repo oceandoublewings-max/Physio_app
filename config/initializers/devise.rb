c4359d69f65e9ece729e1b645cf5fad4fa696147
ocean <ocean.double.wings@gmail.com>
2026-07-22 01:11:50 +0900

    Configure OmniAuth request methods

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -333,3 +333,5 @@ config.omniauth :apple,
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 end
+
+OmniAuth.config.allowed_request_methods = [:post]

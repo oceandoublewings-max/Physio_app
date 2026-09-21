2b4b048fe37a2c052e97a1e0f5a662e2d7747ef0
oceandoublewings-max <ocean.double.wings@gmail.com>
2026-09-22 01:55:53 +0900

    PDFサンプル配信テストを追加

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,4 +5,14 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     get home_index_url
     assert_response :success
   end
+
+  test "serves every sample pdf without login" do
+    %w[bone muscle vessel_nerve physiology kinesiology].each do |kind|
+      get sample_pdf_url(kind: kind, format: :pdf)
+
+      assert_response :success, "#{kind} sample should be available"
+      assert_equal "application/pdf", response.media_type
+      assert response.body.start_with?("%PDF"), "#{kind} sample should contain PDF data"
+    end
+  end
 end

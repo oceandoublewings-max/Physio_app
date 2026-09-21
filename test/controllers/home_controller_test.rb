fc8a3a99c0b13830856c4dd1056a93e6bd5209c0
oceandoublewings-max <ocean.double.wings@gmail.com>
2026-09-22 03:07:32 +0900

    サンプル画像ビューアのテストを追加

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,13 +6,18 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "serves every sample pdf without login" do
+  test "serves every sample viewer and page without login" do
     %w[bone muscle vessel_nerve physiology kinesiology].each do |kind|
-      get sample_pdf_url(kind: kind, format: :pdf)
+      get sample_pdf_url(kind: kind)
 
-      assert_response :success, "#{kind} sample should be available"
-      assert_equal "application/pdf", response.media_type
-      assert response.body.start_with?("%PDF"), "#{kind} sample should contain PDF data"
+      assert_response :success, "#{kind} sample viewer should be available"
+      assert_includes response.body, "sample-viewer"
+
+      get sample_pdf_page_url(kind: kind, page: 1)
+
+      assert_response :success, "#{kind} sample page should be available"
+      assert_equal "image/jpeg", response.media_type
+      assert response.body.b.start_with?("\xFF\xD8".b), "#{kind} sample page should contain JPEG data"
     end
   end
 end
